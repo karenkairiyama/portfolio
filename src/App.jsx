0e098616bef90558342dcef4d07d099b8207e4f8
Karen Kairiyama <karenkairiyama@gmail.com>
2025-09-12 12:25:28 -0300

    First commit 🚀

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+
+const App = () => {
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+      <Hero />
+      <About />
+    </div>
+  );
+};
+
+export default App;
